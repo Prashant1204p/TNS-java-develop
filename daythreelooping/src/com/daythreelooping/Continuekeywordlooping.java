0d9e190889868fbfa26daac17f7f51d8b1fd1e01
PRASHANT <PRASHANT@Prashant>
2025-07-11 10:04:27 +0530

    DAYTHREE

---
+++ daythreelooping/src/com/daythreelooping/Continuekeywordlooping.java
@@ -0,0 +1,17 @@
+package com.daythreelooping;
+
+public class Continuekeywordlooping {
+	public static void main(String[] args)
+	{
+		for(int i=1; i<=100;i++)
+		{
+			if(i%2==1)
+			{
+				continue;
+			
+			}
+			System.out.println("even number="+i);
+		}
+	}
+
+}
---
+++ daythreelooping/src/com/daythreelooping/Whiledowhileloop.java
@@ -0,0 +1,14 @@
+package com.daythreelooping;
+
+public class Whiledowhileloop {
+	public static void main(String[] args)
+	{
+	int i=100;
+	while(i>=1)
+	{
+		System.out.println("i="+i);
+		i--;
+	}
+	}
+}
+
