6d970a4a6c8c02a54eb1e96abed03f03ad6015e4
PRASHANT <PRASHANT@Prashant>
2025-07-11 10:03:22 +0530

    DAYONE

---
+++ dayone/src/com/dayone/Firstprogram.java
@@ -0,0 +1,13 @@
+package com.dayone;
+
+public class Firstprogram{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+		System.out.print("Java Programming");
+		
+		System.out.print(234);
+	
+	}
+	
+}
---
+++ dayone/src/com/dayone/primitivedatatypes/Primitivedatatypes.java
@@ -0,0 +1,21 @@
+package com.dayone.primitivedatatypes;
+
+public class Primitivedatatypes {
+	public static void main(String[] args)
+	{
+		//declaration datatypes
+		//short
+		short s =4227;
+		System.out.println(s);
+		//long
+		long l =112345l;
+		System.out.println(l);
+//char
+		char ch='a'+10;
+		System.out.println(ch);
+		//float
+		float f=3245.2354f;
+		System.out.print(f);
+	}
+
+}
